5a9c482f61ec5cf44f2e5e6cf1aead8baa35944e
ashoss <galaxyash1988@gmail.com>
2024-05-04 12:33:18 +0100

    drop hbm

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,10 +11,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device-V2-ndk.vendor:64 \
     android.hardware.camera.provider-V2-ndk.vendor:64
 
-# HBM
-PRODUCT_PACKAGES += \
-    HbmSVManagerOverlay
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-V2-ndk.vendor:64
